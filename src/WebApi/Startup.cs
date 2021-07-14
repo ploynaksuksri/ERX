89865c0865bbd19cd26b02f641d321b3568259ae
Thongtra Naksuksri <thongtra.naksuksri@myworkpapers.com>
2021-07-14 15:09:19 +0700

    Added swagger

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -30,6 +30,8 @@ namespace WebApi
             services.AddTransient<IChoiceRepository, ChoiceRepository>();
             services.AddTransient<IQuestionManager, QuestionManager>();
             services.AddTransient<IAnswerManager, AnswerManager>();
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +41,16 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
